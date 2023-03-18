9f8b035e3bc31d27b32d2c8a96578c7742fc65a5
Yeshio909 <123256618+Yeshio909@users.noreply.github.com>
2023-03-18 14:26:45 +0530

    Add files via upload

--- launch/joystick.launch.py
+++ launch/joystick.launch.py
@@ -22,7 +22,7 @@ def generate_launch_description():
             executable='teleop_node',
             name='teleop_node',
             parameters=[joy_params, {'use_sim_time': use_sim_time}],
-            remappings=[('/cmd_vel','/cmd_vel_joy')]
+            remappings=[('/cmd_vel','/diff_cont/cmd_vel_unstamped')]
          )
 
     # twist_stamper = Node(
@@ -39,7 +39,7 @@ def generate_launch_description():
             'use_sim_time',
             default_value='false',
             description='Use sim time if true'),
-        joy_node,
-        teleop_node,
-        # twist_stamper       
+            joy_node,
+            teleop_node,
+            # twist_stamper       
     ])
